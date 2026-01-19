7931c3904a2382371d72ba1baab6856e725432e3
lzf <zhenfenglvji@gmail.com>
2026-01-19 21:29:43 +0800

    add a simple sql pool

---
+++ include/sql.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <mysql_driver.h>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+class sqlPools
+{
+private:
+    std::string host_, user_, password_, db_;
+    int poolSize_;
+    std::queue<sql::Connection*> connectionPool_;
+    std::mutex poolMutex_;
+    std::condition_variable poolCondition_;
+public:
+    sqlPools(const std::string& host,const std::string& user,const std::string& passwd,const std::string& db,int poolsize = 10);
+    sql::Connection* getaConnect();
+    void releaseConnect(sql::Connection* conn);
+    ~sqlPools();
+};
---
+++ src/sql.cpp
@@ -0,0 +1,41 @@
+#include "sql.h"
+
+sqlPools::sqlPools(const std::string& host,const std::string& user,const std::string& passwd,const std::string& db,int poolsize = 10):
+    host_(host),
+    user_(user),
+    password_(passwd),
+    db_(db),
+    poolSize_(poolsize)
+{
+    for(int i=0;i<poolSize_;i++){
+        sql::mysql::MySQL_Driver *driver = sql::mysql::get_driver_instance();
+        sql::Connection * con = driver->connect(host_,user_,password_);
+        con->setSchema(db_);
+        connectionPool_.push(con);
+    }
+}
+    
+
+sql::Connection* sqlPools::getaConnect(){
+    std::unique_lock<std::mutex> lock(poolMutex_);
+    while(connectionPool_.empty()){
+        poolCondition_.wait(lock);
+    }
+    sql::Connection *conn = connectionPool_.front();
+    connectionPool_.pop();
+    return conn;
+}
+    void sqlPools::releaseConnect(sql::Connection* conn){
+        std::unique_lock<std::mutex> lock(poolMutex_);
+        connectionPool_.push(conn);
+        poolCondition_.notify_one();
+
+    }
+    sqlPools::~sqlPools(){
+        while (!connectionPool_.empty())
+        {
+            sql::Connection* c = connectionPool_.front();
+            connectionPool_.pop();
+            delete c;
+        }    
+    }
